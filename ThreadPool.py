a07b8ab9d9aacad14dad6b36e4c2b05362def1de
root <root@localhost.localdomain>
2015-05-18 13:45:07 +0100

    Added searching capabilites, the ability to specify plugins and the ability to dictate the size of the thread pool

--- ThreadPool.py
+++ ThreadPool.py
@@ -2,7 +2,7 @@ from Queue import Queue
 from threading import Thread
 
 class Worker(Thread):
-    """Thread executing tasks from a given tasks queue"""
+    '''Thread executing tasks from a given tasks queue'''
     def __init__(self, tasks):
         Thread.__init__(self)
         self.tasks = tasks
@@ -17,17 +17,17 @@ class Worker(Thread):
             self.tasks.task_done()
 
 class ThreadPool:
-    """Pool of threads consuming tasks from a queue"""
+    '''Pool of threads consuming tasks from a queue'''
     def __init__(self, num_threads):
         self.tasks = Queue(num_threads)
         for _ in range(num_threads): Worker(self.tasks)
 
     def add_task(self, func, *args, **kargs):
-        """Add a task to the queue"""
+        '''Add a task to the queue'''
         self.tasks.put((func, args, kargs))
 
     def wait_completion(self):
-        """Wait for completion of all the tasks in the queue"""
+        '''Wait for completion of all the tasks in the queue'''
         self.tasks.join()
 
 if __name__ == '__main__':
